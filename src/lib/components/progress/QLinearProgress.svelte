def9982833fdc622938f174e62530306a33ddc8f
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2023-05-29 02:32:40 +0200

    add a minimal version of checkbox, radio and linear progress components.

---
+++ src/lib/components/progress/QLinearProgress.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let value: number = 0;
+</script>
+
+<div class="small-space border">
+  <div class="progress left" style={`clip-path: polygon(0% 0%, 0% 100%, ${value}% 100%, ${value}% 0%);`}></div>
+</div>
---
+++ src/lib/components/radio/QRadio.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let val: string = '';
+  export let label: string = '';
+  export let selected: any;
+</script>
+
+<label class="radio">
+  <input type="radio" bind:group={selected} value={val} />
+  <span>{label}</span>
+</label>
