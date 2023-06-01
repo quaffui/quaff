ca304e96d7589f34151094fdb9437853e073f8cc
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-06-01 15:18:47 +0200

    Created a basic use-router-link

---
+++ src/lib/composables/use-router-link.ts
@@ -0,0 +1,31 @@
+import { stringifyClasses } from "$lib/utils/props";
+
+export interface UseRouterLinkProps {
+  href?: string;
+  to?: string;
+  disable: boolean;
+  activeClass?: string;
+  replace: boolean;
+}
+
+export const UseRouterLinkPropsDefaults: UseRouterLinkProps = {
+  href: undefined,
+  to: undefined,
+  disable: false,
+  activeClass: undefined,
+  replace: false,
+};
+
+export default function <T extends UseRouterLinkProps>(props: T) {
+  const linkAttributes = {
+    href: props.to || props.href,
+    "data-sveltkit-reload": props.replace === true ? "" : undefined,
+  };
+
+  const linkClasses = stringifyClasses(["q-link", props.disable && "disable"]);
+
+  return {
+    linkAttributes,
+    classes: linkClasses,
+  };
+}
