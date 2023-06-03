11c1644d2b23dbf3961a550df7f68ab3dd77ea3a
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-06-03 18:15:30 +0200

    Changed classes and added restProps

--- src/lib/components/tooltip/QTooltip.svelte
+++ src/lib/components/tooltip/QTooltip.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+  import { stringifyClasses } from "$lib/utils/props";
   import { type QTooltipProps } from "./props";
 
   export let position: QTooltipProps["position"] = "bottom",
     userClasses: QTooltipProps["userClasses"] = "";
   export { userClasses as class };
+
+  $: classes = stringifyClasses(["tooltip", position, userClasses]);
 </script>
 
-<div class={`tooltip ${position}`}>
+<div class={classes} {...$$restProps}>
   <slot />
 </div>
