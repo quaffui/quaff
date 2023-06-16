8eaab57ce0338366fde7a74cff4eaddb4ae9a511
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-06-16 16:13:40 +0200

    Wrote docs for breadcrumbs

--- src/lib/components/breadcrumbs/docs.ts
+++ src/lib/components/breadcrumbs/docs.ts
@@ -5,10 +5,50 @@ export let QBreadcrumbsDocs: QComponentDocs = {
   description:
     "Breadcrumbs are mostly used as a navigation aid. They allow users to keep track of their location within the page.",
   docs: {
-    props: [],
-    slots: [],
+    props: [
+      {
+        name: "separator",
+        type: "string",
+        default: "/",
+        description:
+          'What to use as a separator. To use an icon, prefix with "icon:" followed by the name of the icon.',
+      },
+      {
+        name: "gutter",
+        type: "QBreadcrumbsGutterOptions",
+        default: "sm",
+        description: "Space around separators.",
+        clickableType: true,
+      },
+      {
+        name: "activeColor",
+        type: "string",
+        default: "primary",
+        description:
+          "Color to use for the active breadcrumb element. See <link to colors docs> to see what colors can be used.",
+      },
+      {
+        name: "separatorColor",
+        type: "string",
+        default: "outline",
+        description:
+          "Color to use for the separators. See <link to colors docs> to see what colors can be used.",
+      },
+    ],
+    slots: [
+      {
+        name: "default",
+        description:
+          "The default slot is used to inserts breadcrumb elements. Use QBreadcrumbsEl to insert elements.",
+      },
+    ],
     methods: [],
     events: [],
-    types: [],
+    types: [
+      {
+        name: "QBreadcrumbsGutterOptions",
+        description: "none | sm | md | lg",
+      },
+    ],
   },
 };
