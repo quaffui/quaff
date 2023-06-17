afba883cff7450766cb9353d2651a2b01bc43c24
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-06-17 19:10:39 +0200

    Fixed QChackbox documentation

--- src/routes/components/checkbox/+page.svelte
+++ src/routes/components/checkbox/+page.svelte
@@ -1,26 +1,26 @@
 <script lang="ts">
   import { QCheckbox } from "$lib";
   import { QCheckboxDocs } from "$lib/components/checkbox/docs";
-  import ApiDoc from "$lib/components/private/ApiDoc.svelte";
-  import ApiDocSection from "$lib/components/private/ApiDocSection.svelte";
+  import QDocs from "$lib/components/private/QDocs.svelte";
+  import QDocsSection from "$lib/components/private/QDocsSection.svelte";
   let value1 = false;
 </script>
 
-<ApiDoc QComponentDocs={QCheckboxDocs} usagePath="checkbox">
+<QDocs QComponentDocs={QCheckboxDocs} usagePath="checkbox">
   <QCheckbox slot="display" label="I agree to the terms and conditions" bind:value={value1} />
 
   <div slot="usage">
-    <ApiDocSection title="Default Checkbox">
+    <QDocsSection title="Default Checkbox">
       <QCheckbox class="q-ma-sm" bind:value={value1} />
       <QCheckbox class="q-ma-sm" label="With label" bind:value={value1} />
-    </ApiDocSection>
+    </QDocsSection>
 
-    <ApiDocSection title="Checked Checkbox">
+    <QDocsSection title="Checked Checkbox">
       <QCheckbox class="q-ma-sm" label="Checkbox checked by default" value={true} />
-    </ApiDocSection>
+    </QDocsSection>
 
-    <ApiDocSection title="Disabled">
+    <QDocsSection title="Disabled">
       <QCheckbox class="q-ma-sm" label="Checkbox with disable prop" value={true} disable />
-    </ApiDocSection>
+    </QDocsSection>
   </div>
-</ApiDoc>
+</QDocs>
