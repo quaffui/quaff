60de13022ae72ed0b420d5ea97590a40318a56db
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-06-20 14:32:16 +0200

    Added avatar text to examples

--- src/routes/components/avatar/+page.svelte
+++ src/routes/components/avatar/+page.svelte
@@ -9,25 +9,34 @@
   <QAvatar slot="display" src="/cocktail.jpg" size="5rem" />
 
   <div slot="usage">
+    <QDocsSection title="Default slot">
+      <div class="flex q-gap-lg middle-align">
+        <QAvatar class="green">GR</QAvatar>
+        <QAvatar class="red5">Rgfojzfigzbfoa</QAvatar>
+        <QAvatar class="primary">PR</QAvatar>
+        <QAvatar class="primary-container">PC</QAvatar>
+      </div>
+    </QDocsSection>
+
     <QDocsSection title="Avatar Sizes">
-      <div class="flex avatar-gap" style="align-items: center">
-        <QAvatar src="/cocktail.jpg" shape="circle" size="xs" />
-        <QAvatar src="/cocktail.jpg" shape="circle" size="sm" />
-        <QAvatar src="/cocktail.jpg" shape="circle" />
-        <QAvatar src="/cocktail.jpg" shape="circle" size="lg" />
-        <QAvatar src="/cocktail.jpg" shape="circle" size="xl" />
+      <div class="flex q-gap-lg middle-align">
+        <QAvatar src="/cocktail.jpg" size="xs" />
+        <QAvatar src="/cocktail.jpg" size="sm" />
+        <QAvatar src="/cocktail.jpg" />
+        <QAvatar src="/cocktail.jpg" size="lg" />
+        <QAvatar src="/cocktail.jpg" size="xl" />
       </div>
     </QDocsSection>
 
     <QDocsSection title="Custom Avatar Sizes">
-      <div class="flex avatar-gap" style="align-items: center">
-        <QAvatar src="/cocktail.jpg" shape="circle" size="5rem" />
+      <div class="flex q-gap-lg middle-align">
+        <QAvatar src="/cocktail.jpg" size="5rem" />
       </div>
     </QDocsSection>
 
     <QDocsSection title="Avatar Shapes">
-      <div class="flex avatar-gap" style="align-items: center">
-        <QAvatar src="/cocktail.jpg" shape="circle" />
+      <div class="flex q-gap-lg middle-align">
+        <QAvatar src="/cocktail.jpg" />
         <QAvatar src="/cocktail.jpg" shape="rounded" />
         <QAvatar src="/cocktail.jpg" shape="top-round" />
         <QAvatar src="/cocktail.jpg" shape="bottom-left-round" />
@@ -35,8 +44,8 @@
     </QDocsSection>
 
     <QDocsSection title="Avatar Videos">
-      <div class="flex avatar-gap" style="align-items: center">
-        <QAvatar src="/sea.mp4" shape="circle" size="lg" video />
+      <div class="flex q-gap-lg middle-align">
+        <QAvatar src="/sea.mp4" size="lg" video />
         <QAvatar src="/sea.mp4" shape="rounded" size="lg" video />
         <QAvatar src="/sea.mp4" shape="left-round" size="lg" video />
         <QAvatar src="/sea.mp4" shape="top-right-round" size="lg" video />
@@ -44,9 +53,3 @@
     </QDocsSection>
   </div>
 </QDocs>
-
-<style lang="scss">
-  .avatar-gap {
-    gap: 2em;
-  }
-</style>
