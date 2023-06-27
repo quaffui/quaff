d319a14e9b916e4417fbebef444d9545fea6162c
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-06-27 14:07:04 +0200

    Added QBreadCrumbs docs

--- src/lib/components/breadcrumbs/docs.ts
+++ src/lib/components/breadcrumbs/docs.ts
@@ -1,40 +1,12 @@
 import type { QComponentDocs } from "$utils/types";
+import { QBreadcrumbsDocsProps } from "./docs.props";
 
 export let QBreadcrumbsDocs: QComponentDocs = {
   name: "QBreadcrumbs",
   description:
     "Breadcrumbs are mostly used as a navigation aid. They allow users to keep track of their location within the page.",
   docs: {
-    props: [
-      {
-        name: "separator",
-        type: "string",
-        default: "/",
-        description:
-          'What to use as a separator. To use an icon, prefix with "icon:" followed by the name of the icon.',
-      },
-      {
-        name: "gutter",
-        type: "QBreadcrumbsGutterOptions",
-        default: "sm",
-        description: "Space around separators.",
-        clickableType: true,
-      },
-      {
-        name: "activeColor",
-        type: "string",
-        default: "primary",
-        description:
-          "Color to use for the active breadcrumb element. See <link to colors docs> to see what colors can be used.",
-      },
-      {
-        name: "separatorColor",
-        type: "string",
-        default: "outline",
-        description:
-          "Color to use for the separators. See <link to colors docs> to see what colors can be used.",
-      },
-    ],
+    props: QBreadcrumbsDocsProps,
     slots: [
       {
         name: "default",
--- src/lib/components/breadcrumbs/props.ts
+++ src/lib/components/breadcrumbs/props.ts
@@ -1,43 +1,67 @@
-import { NativePropsDefaults, type NativeProps } from "$lib/utils/types";
+import type { NativeProps } from "$lib/utils/types";
 
 export type QBreadcrumbsGutterOptions = "none" | "sm" | "md" | "lg";
 
 export interface QBreadcrumbsProps extends NativeProps {
-  separator: string;
-  gutter: QBreadcrumbsGutterOptions;
+  /**
+   * Separator to use between the breadcrumb elements. To use an icon, prefix with "icon:" followed by the name of the icon.
+   * @default /
+   */
+  separator?: string;
 
-  activeColor: string;
-  separatorColor: string;
+  /**
+   * Space around separators.
+   * @default sm
+   */
+  gutter?: QBreadcrumbsGutterOptions;
+
+  /**
+   * Color to use for the active breadcrumb element. See <link to colors docs> to see what colors can be used.
+   * @default primary
+   */
+  activeColor?: string;
+
+  /**
+   * Color to use for the separators. See <link to colors docs> to see what colors can be used.
+   * @default outline
+   */
+  separatorColor?: string;
 }
 
 export interface QBreadcrumbsElProps extends NativeProps {
-  label: string;
-  icon?: string;
-  tag: string;
+  /**
+   * Text to use for the breadcrumb element. If default slot is defined, the label will be overwritten by it.
+   * @default ""
+   */
+  label?: string;
 
+  /**
+   * Name of the leading icon for the breadcrumb element. The icon prop overwrites to icon slot.
+   * @default undefined
+   */
+  icon?: string;
+
+  /**
+   * Tag to use for the breadcrumb element.
+   * @default "div"
+   */
+  tag?: string;
+
+  /**
+   * Makes the breadcrumb element navigational. Can be used with the router (e.g to="/home") or as a normal href attribute (e.g to="#section-id")
+   * @default undefined
+   */
   to?: string;
+
+  /**
+   * Also makes the breadcrumb element navigational. Can be used with the router (e.g to="/home") or as a normal href attribute (e.g to="#section-id")
+   * @default undefined
+   */
   href?: string;
+
+  /**
+   * Class to apply to the breadcrumb element when the route is active.
+   * @default "active"
+   */
   activeClass?: string;
 }
-
-export const QBreadcrumbsPropsDefaults: QBreadcrumbsProps = {
-  separator: "/",
-  gutter: "sm",
-
-  activeColor: "primary",
-  separatorColor: "outline",
-
-  ...NativePropsDefaults,
-};
-
-export const QBreadcrumbsElPropsDefaults: QBreadcrumbsElProps = {
-  label: "",
-  icon: undefined,
-  tag: "div",
-
-  to: undefined,
-  href: undefined,
-  activeClass: "active",
-
-  ...NativePropsDefaults,
-};
