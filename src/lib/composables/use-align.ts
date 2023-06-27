6b67ce5340afd6b88cba717a42cc85f40ca568c1
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-06-27 14:51:51 +0200

    Added a UseAlignOptions type

--- src/lib/composables/use-align.ts
+++ src/lib/composables/use-align.ts
@@ -9,19 +9,21 @@ export type JustifyOptions =
 
 export type AlignOptions = "top" | "middle" | "bottom";
 
+export type UseAlignOptions =
+  | `${AlignOptions}`
+  | `${JustifyOptions}`
+  | `${AlignOptions} ${JustifyOptions}`
+  | `${JustifyOptions} ${AlignOptions}`;
+
 export interface UseAlignProps {
-  align?:
-    | `${AlignOptions}`
-    | `${JustifyOptions}`
-    | `${AlignOptions} ${JustifyOptions}`
-    | `${JustifyOptions} ${AlignOptions}`;
+  align?: UseAlignOptions;
 }
 
 export const UseAlignPropsDefaults: UseAlignProps = {
   align: "top left",
 };
 
-export default function useAlign(align: UseAlignProps["align"] = "top left") {
+export default function useAlign(align: UseAlignOptions = "top left") {
   const alignClass = align
     .split(" ")
     .map((a) => `${a}-align`)
