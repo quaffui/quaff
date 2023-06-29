f3b3e608ef63f5ffb68d91401474cae0646d0213
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-06-29 17:09:03 +0200

    Added height prop and moved styles to index.scss

--- src/lib/components/toolbar/QToolbar.svelte
+++ src/lib/components/toolbar/QToolbar.svelte
@@ -3,10 +3,13 @@
   import { getContext } from "svelte";
   import type { QToolbarProps } from "./props";
   import type { AppbarContext } from "../layout/QLayout.svelte";
+  import { isNumber } from "$lib/utils/types";
 
   export let inset: QToolbarProps["inset"] = false,
-    userClasses: QToolbarProps["userClasses"] = undefined;
-  export { userClasses as class };
+    height: QToolbarProps["height"] = "64px",
+    userClasses: QToolbarProps["userClasses"] = undefined,
+    userStyles: QToolbarProps["userStyles"] = undefined;
+  export { userClasses as class, userStyles as style };
 
   $: ctx = getContext<AppbarContext | undefined>("header");
 
@@ -18,21 +21,19 @@
     inset && "q-toolbar--inset",
     userClasses,
   ]);
+
+  $: heightStyle = ctx === undefined ? (isNumber(height) ? `${height}px` : height) : undefined;
+
+  $: style = createStyles(
+    {
+      "--header-height": heightStyle,
+    },
+    userStyles
+  );
 </script>
 
-<header class={classes} role="toolbar" {...$$restProps}>
+<header class={classes} role="toolbar" {...$$restProps} {style}>
   <nav>
     <slot />
   </nav>
 </header>
-
-<style lang="scss">
-  .q-toolbar {
-    width: 100%;
-    height: 64px;
-    transition: all var(--speed3);
-    &--inset {
-      padding-left: 58px;
-    }
-  }
-</style>
--- src/lib/components/toolbar/props.ts
+++ src/lib/components/toolbar/props.ts
@@ -3,9 +3,11 @@ import { NativePropsDefaults } from "$lib/utils/types";
 
 export interface QToolbarProps extends NativeProps {
   inset: boolean;
+  height: string | number;
 }
 
 export const QToolbarPropsDefaults: QToolbarProps = {
   inset: false,
+  height: "64px",
   ...NativePropsDefaults,
 };
