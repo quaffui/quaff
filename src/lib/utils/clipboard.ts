b836f9fce4011e946315b39c179dfc414ef52c38
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-07-02 12:18:58 +0200

    Added a clipboard copy util

---
+++ src/lib/utils/clipboard.ts
@@ -0,0 +1,9 @@
+async function copy(text: string) {
+  if (navigator.clipboard.write) {
+    let blob = new Blob([text], { type: "text/plain" });
+    let item = new ClipboardItem({ "text/plain": blob });
+    await navigator.clipboard.write([item]);
+  } else {
+    await navigator.clipboard.writeText(text);
+  }
+}
