5616d0d1a5eeb8207827c482af5dd00de2f2c5eb
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-07-04 17:21:35 +0200

    QDialog: Created docs

--- src/routes/components/dialog/+page.svelte
+++ src/routes/components/dialog/+page.svelte
@@ -7,11 +7,56 @@
   import QDialog from "$lib/components/dialog/QDialog.svelte";
   import { QDialogDocs } from "$lib/components/dialog/docs";
   import QDocs from "$lib/components/private/QDocs.svelte";
+  import QDocsSection from "$lib/components/private/QDocsSection.svelte";
   import QToggle from "$lib/components/toggle/QToggle.svelte";
+  import DialogContent from "./DialogContent.svelte";
+  import snippets from "./docs.snippets";
 
-  let exampleDialog = false;
-  let exampleToggles = [true, true, true];
-  let exampleCheckbox = false;
+  interface Dialog {
+    el: QDialog | null;
+    value: boolean;
+  }
+
+  let exampleDialog: Dialog = {
+    el: null,
+    value: false,
+  };
+  let topDialog: Dialog = {
+    el: null,
+    value: false,
+  };
+  let defaultDialog: Dialog = {
+    el: null,
+    value: false,
+  };
+  let rightDialog: Dialog = {
+    el: null,
+    value: false,
+  };
+  let bottomDialog: Dialog = {
+    el: null,
+    value: false,
+  };
+  let leftDialog: Dialog = {
+    el: null,
+    value: false,
+  };
+  let modalDialog: Dialog = {
+    el: null,
+    value: false,
+  };
+  let persistentDialog: Dialog = {
+    el: null,
+    value: false,
+  };
+  let fullscreenDialog: Dialog = {
+    el: null,
+    value: false,
+  };
+  let methodsDialog: Dialog = {
+    el: null,
+    value: false,
+  };
 </script>
 
 <QDocs QComponentDocs={QDialogDocs}>
@@ -20,36 +65,81 @@
     btnContent="Open cookie settings"
     modal
     persistent
-    bind:value={exampleDialog}
-    on:btnClick={() => (exampleDialog = !exampleDialog)}
+    bind:value={exampleDialog.value}
+    bind:this={exampleDialog.el}
   >
-    <QCard title="Cookie settings" flat>
-      <QCardSection>
-        {#each exampleToggles as toggle, index}
-          <div class="flex between-align q-my-sm">
-            {#if index === 0}
-              <h6 class="small">Cookies {index + 1} (always active)</h6>
-              <QToggle bind:value={toggle} disable />
-            {:else}
-              <h6 class="small">Cookies {index + 1}</h6>
-              <QToggle bind:value={toggle} />
-            {/if}
-          </div>
-        {/each}
-      </QCardSection>
-      <QCardSection>
-        <div class="flex middle-align" style="white-space: pre">
-          I've read the <span class="primary-text" style="cursor: pointer">
-            terms and conditions
-          </span>
-          <QCheckbox class="q-ml-sm" bind:value={exampleCheckbox} />
-        </div>
-      </QCardSection>
-      <QCardActions align="right">
-        <QBtn on:click={() => (exampleDialog = !exampleDialog)} disable={!exampleCheckbox}>
-          Save
-        </QBtn>
-      </QCardActions>
-    </QCard>
+    <DialogContent dialogEl={exampleDialog.el} />
   </QDialog>
+
+  <div slot="usage">
+    <QDocsSection {snippets} title="Positions">
+      <QDialog bind:this={defaultDialog.el} bind:value={defaultDialog.value} btnContent="Default">
+        <DialogContent dialogEl={defaultDialog.el} />
+      </QDialog>
+      <QDialog
+        bind:this={topDialog.el}
+        bind:value={topDialog.value}
+        btnContent="Top"
+        position="top"
+      >
+        <DialogContent dialogEl={topDialog.el} />
+      </QDialog>
+      <QDialog
+        bind:this={rightDialog.el}
+        bind:value={rightDialog.value}
+        btnContent="Right"
+        position="right"
+      >
+        <DialogContent dialogEl={rightDialog.el} />
+      </QDialog>
+      <QDialog
+        bind:this={bottomDialog.el}
+        bind:value={bottomDialog.value}
+        btnContent="Bottom"
+        position="bottom"
+      >
+        <DialogContent dialogEl={bottomDialog.el} />
+      </QDialog>
+      <QDialog
+        bind:this={leftDialog.el}
+        bind:value={leftDialog.value}
+        btnContent="Left"
+        position="left"
+      >
+        <DialogContent dialogEl={leftDialog.el} />
+      </QDialog>
+    </QDocsSection>
+
+    <QDocsSection {snippets} title="Types">
+      <QDialog bind:this={modalDialog.el} bind:value={modalDialog.value} btnContent="Modal" modal>
+        <DialogContent dialogEl={modalDialog.el} />
+      </QDialog>
+      <QDialog
+        bind:this={fullscreenDialog.el}
+        bind:value={fullscreenDialog.value}
+        btnContent="Fullscreen"
+        fullscreen
+      >
+        <DialogContent dialogEl={fullscreenDialog.el} />
+      </QDialog>
+      <QDialog
+        bind:this={persistentDialog.el}
+        bind:value={persistentDialog.value}
+        btnContent="Persistent"
+        persistent
+        modal
+      >
+        <DialogContent dialogEl={persistentDialog.el} />
+      </QDialog>
+    </QDocsSection>
+
+    <QDocsSection {snippets} title="Methods and noBtn">
+      <QDialog bind:this={methodsDialog.el} bind:value={methodsDialog.value} noBtn persistent>
+        <DialogContent dialogEl={methodsDialog.el} />
+      </QDialog>
+      <QBtn on:click={() => methodsDialog.el?.show()}>Show</QBtn>
+      <QBtn on:click={() => methodsDialog.el?.hide()}>Hide</QBtn>
+      <QBtn on:click={() => methodsDialog.el?.toggle()}>Toggle</QBtn>
+    </QDocsSection>
+  </div>
 </QDocs>
---
+++ src/routes/components/dialog/DialogContent.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { QCard, QCardSection, QToggle, QCheckbox, QCardActions, QBtn, QDialog } from "$lib";
+
+  let exampleToggles = [true, true, true];
+  let exampleCheckbox = false;
+
+  export let dialogEl: QDialog | null;
+</script>
+
+<QCard title="Cookie settings" flat>
+  <QCardSection>
+    {#each exampleToggles as toggle, index}
+      <div class="flex between-align q-my-sm">
+        {#if index === 0}
+          <h6 class="small">Cookies {index + 1} (always active)</h6>
+          <QToggle bind:value={toggle} disable />
+        {:else}
+          <h6 class="small">Cookies {index + 1}</h6>
+          <QToggle bind:value={toggle} />
+        {/if}
+      </div>
+    {/each}
+  </QCardSection>
+  <QCardSection>
+    <div class="flex middle-align" style="white-space: pre">
+      I've read the <span class="primary-text" style="cursor: pointer"> terms and conditions </span>
+      <QCheckbox class="q-ml-sm" bind:value={exampleCheckbox} />
+    </div>
+  </QCardSection>
+  <QCardActions align="right">
+    <QBtn on:click={() => dialogEl?.hide()}>Cancel</QBtn>
+    <QBtn on:click={() => dialogEl?.hide()} disable={!exampleCheckbox}>Save</QBtn>
+  </QCardActions>
+</QCard>
