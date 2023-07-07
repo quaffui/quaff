5eecfd44a585967071eaa8a140f39142312de867
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-07-07 15:04:14 +0200

    Added a convertCase util function

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -1,9 +1,80 @@
 export function capitalize(str: string, eachWord: boolean = false) {
   const cap = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
 
-  if (eachWord === true) {
-    return str.split(" ").map(cap).join(" ");
-  } else {
-    return cap(str);
+  return eachWord ? str.split(" ").map(cap).join(" ") : cap(str);
+}
+
+const cases = {
+  camel: "",
+  pascal: "",
+  kebab: "-",
+  snake: "_",
+};
+
+function convertToPascalCase(str: string, delimeter: string) {
+  return str
+    .split(delimeter)
+    .map((word) => capitalize(word))
+    .join("");
+}
+
+function convertToCamelCase(str: string, delimeter: string) {
+  return str
+    .split(delimeter)
+    .map((word, index) => (index === 0 ? word : capitalize(word)))
+    .join("");
+}
+
+function convertToKebabSnakeCase(str: string, caseType: keyof typeof cases) {
+  return str
+    .split("")
+    .map((letter) => (/[A-Z]/.test(letter) ? `${cases[caseType]}${letter.toLowerCase()}` : letter))
+    .join("");
+}
+
+export function convertCase(str: string, fromCase: keyof typeof cases, toCase: keyof typeof cases) {
+  switch (fromCase) {
+    case "camel":
+      if (toCase === "pascal") {
+        return capitalize(str);
+      }
+      if (toCase === "kebab" || toCase === "snake") {
+        return convertToKebabSnakeCase(str, toCase);
+      }
+      break;
+    case "pascal":
+      if (toCase === "camel") {
+        return str.charAt(0).toLowerCase() + str.slice(1);
+      }
+      if (toCase === "kebab" || toCase === "snake") {
+        return convertToKebabSnakeCase(str, toCase);
+      }
+      break;
+    case "snake":
+      if (toCase === "kebab") {
+        return str.replaceAll("_", "-");
+      }
+      if (toCase === "camel") {
+        return convertToCamelCase(str, "_");
+      }
+      if (toCase === "pascal") {
+        return convertToPascalCase(str, "_");
+      }
+      break;
+    case "kebab":
+      if (toCase === "snake") {
+        return str.replaceAll("-", "_");
+      }
+      if (toCase === "camel") {
+        return convertToCamelCase(str, "-");
+      }
+      if (toCase === "pascal") {
+        return convertToPascalCase(str, "-");
+      }
+      break;
+    default:
+      return str;
   }
+
+  return str;
 }
