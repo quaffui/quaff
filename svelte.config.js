f5ccb5c7cb8e3fd2c75601cb7dd13dc09e73bcf2
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2023-08-03 01:08:15 +0200

    fix: svelte.config.js: added aliases.

--- svelte.config.js
+++ svelte.config.js
@@ -16,6 +16,15 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    alias: {
+      $lib: "./src/lib",
+      $components: "./src/lib/components",
+      $composables: "./src/lib/composables",
+      $utils: "./src/lib/utils",
+      $css: "./src/lib/css",
+      $stores: "./src/lib/stores",
+      $helpers: "./src/lib/helpers",
+    },
   },
 };
 
