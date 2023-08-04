b761927d240ad5e5efd0406dabf63bfc6dfed64c
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2023-08-04 23:53:06 +0200

    feat: compile to css, add css to exports.

---
+++ vite.config.scss.ts
@@ -0,0 +1,21 @@
+import { resolve } from "path";
+
+export default {
+  resolve: {
+    alias: {
+      $lib: resolve(__dirname, "src/lib"),
+      $components: resolve(__dirname, "./src/lib/components"),
+      $css: resolve(__dirname, "./src/lib/css"),
+    },
+  },
+  build: {
+    emptyOutDir: false,
+    rollupOptions: {
+      input: "src/lib/css/index.scss",
+      output: {
+        dir: "dist/css",
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+};
