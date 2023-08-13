5f50e17255904a93da96a6d39bdd9adcf1b50b1d
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-08-13 14:16:33 +0200

    feat: Added some keyboard event utils

--- src/lib/utils/events.ts
+++ src/lib/utils/events.ts
@@ -1,3 +1,24 @@
-export function isActivationKey(e: KeyboardEvent) {
+export type Direction = "previous" | "next";
+
+// Keyboard events
+export function isActivationKey(
+  e: KeyboardEvent
+): e is KeyboardEvent & { code: "Enter" | "Space" } {
   return e.code === "Enter" || e.code === "Space";
+}
+
+export function isArrowKey(
+  e: KeyboardEvent
+): e is KeyboardEvent & { code: "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight" } {
+  return e.code.startsWith("Arrow");
+}
+
+export function isTabKey(e: KeyboardEvent): e is KeyboardEvent & { code: "Tab" } {
+  return e.code === "Tab";
+}
+
+export function getDirection(
+  e: KeyboardEvent & { code: "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight" }
+): Direction {
+  return e.code === "ArrowDown" || e.code === "ArrowRight" ? "next" : "previous";
 }
