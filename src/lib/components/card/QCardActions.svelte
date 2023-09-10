dce930333cbd0617b9ee91040b9fc88e025cdf9d
KilDesu <91275372+KilDesu@users.noreply.github.com>
2023-09-10 13:36:12 +0200

    refactor(QCard): refactored classes and styles

--- src/lib/components/card/QCardActions.svelte
+++ src/lib/components/card/QCardActions.svelte
@@ -1,23 +1,19 @@
 <script lang="ts">
   import {useAlign} from "$lib/composables";
-  import { createClasses } from "$lib/utils";
   import type { QCardActionsProps } from "./props";
 
   export let align: QCardActionsProps["align"] = undefined,
     vertical: QCardActionsProps["vertical"] = false,
-    userClasses: QCardActionsProps["userClasses"] = undefined;
+    userClasses: QCardActionsProps["userClasses"] = "";
   export { userClasses as class };
 
   $: alignClass = useAlign(align);
-
-  $: classes = createClasses([vertical && "vertical"], {
-    component: "q-card",
-    element: "actions",
-    quaffClasses: [alignClass],
-    userClasses,
-  });
 </script>
 
-<nav class={classes} {...$$restProps}>
+<nav
+  class="q-card__actions {alignClass} {userClasses}"
+  class:q-card__actions--vertical={vertical}
+  {...$$restProps}
+>
   <slot />
 </nav>
