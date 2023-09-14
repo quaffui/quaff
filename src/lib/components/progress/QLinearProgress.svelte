c36ae7ca99b7bf58c6f4971b18627e35d39fed51
therealcoder1337 <78889972+therealcoder1337@users.noreply.github.com>
2023-09-14 16:12:04 +0200

    refactor(QLinearProgress): inline styles to css classes where possible.

--- src/lib/components/progress/QLinearProgress.svelte
+++ src/lib/components/progress/QLinearProgress.svelte
@@ -1,28 +1,17 @@
 <script lang="ts">
-  import { createStyles } from "$lib/utils";
   import type { QLinearProgressProps } from "./props";
 
   export let value: QLinearProgressProps["value"] = 0,
     from: QLinearProgressProps["from"] = "left",
     rounded: QLinearProgressProps["rounded"] = false,
-    userClasses: QLinearProgressProps["userClasses"] = undefined,
-    userStyles: QLinearProgressProps["userStyles"] = undefined;
-  export { userClasses as class, userStyles as style };
-
-  $: roundedStyle = {
-    borderRadius: rounded ? "12px" : "0",
-  };
-
-  $: containerStyle = createStyles(roundedStyle, userStyles);
-  $: progressStyle =
-    from === "right"
-      ? `clip-path: polygon(100% 0%, 100% 100%, ${100 - value}% 100%, ${100 - value}% 0%);`
-      : `clip-path: polygon(0% 0%, 0% 100%, ${value}% 100%, ${value}% 0%);`;
+    userClasses: QLinearProgressProps["userClasses"] = "";
+  export { userClasses as class };
 </script>
 
 <div
   class="q-linear-progress {userClasses}"
-  style={containerStyle}
+  class:q-linear-progress--rounded={rounded}
+  class:q-linear-progress--right={from === "right"}
   role="progressbar"
   aria-valuenow={value}
   aria-valuemin={0}
@@ -32,6 +21,6 @@
   <div
     class="q-linear-progress__progress"
     class:q-linear-progress__progress--right={from === "right"}
-    style={progressStyle}
+    style="--progress:{value}%"
   />
 </div>
