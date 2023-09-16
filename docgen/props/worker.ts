df7d5f5576575cf5fce4d56aaea662e452567e06
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2023-09-16 04:40:20 +0200

    feat(docgen): now running multithreaded.

---
+++ docgen/props/worker.ts
@@ -0,0 +1,53 @@
+import { writeFile } from "fs/promises";
+import { MessagePort, parentPort } from "worker_threads";
+import path from "path";
+import prettier from "prettier";
+import parseInterface from "./parse-interface.js";
+import parseTypes from "../types/parseTypes.js";
+
+function assertHasParentPort(parentPort: MessagePort | null): asserts parentPort is MessagePort {
+  if (!parentPort) {
+    throw new Error("missing parentPort");
+  }
+}
+
+async function formatCode(code: string) {
+  const options = await prettier.resolveConfig(path.join(process.cwd(), ".prettierrc"));
+  return prettier.format(code, { ...options, parser: "typescript" });
+}
+
+assertHasParentPort(parentPort);
+
+parentPort.on("message", async (workerData) => {
+  const { propsFilePath, docsPropsFilePath, hashProps } = workerData;
+
+  console.log("processing", propsFilePath);
+  const parsedInterface = parseInterface(propsFilePath);
+  const types = await parseTypes(propsFilePath);
+
+  let contents = "";
+
+  Object.keys(parsedInterface).forEach((varName) => {
+    const interfaceResults = parsedInterface[varName];
+
+    contents += `export const ${varName.replace(/Props$/, "DocsProps")} = ${JSON.stringify(
+      interfaceResults,
+      null,
+      2
+    )};\n\n`;
+  });
+
+  const formatted = await formatCode(contents);
+  const formattedWithComment = [
+    "// AUTO GENERATED FILE - DO NOT MODIFY OR DELETE",
+    `// @quaffHash ${hashProps}`,
+    "",
+    formatted,
+  ].join("\n");
+
+  await writeFile(docsPropsFilePath, formattedWithComment, "utf8");
+
+  assertHasParentPort(parentPort);
+
+  parentPort.postMessage({ event: "finished", types });
+});
