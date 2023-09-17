b533038d199b2e00dd0a136f0e7b29e589448a67
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2023-09-18 01:07:21 +0200

    fix: add script for writing version to circumvent svelte-check issues.

--- src/dev/version-plugin.ts
+++ src/dev/version-plugin.ts
@@ -1,19 +1,11 @@
-import { readFile, writeFile } from "fs/promises";
-import path from "path";
+import writeVersion from "./writeVersion.js";
 import type { Plugin } from "vite";
 
 function versionPlugin(): Plugin {
   return {
     name: "version-plugin",
-    async configResolved(config) {
-      const data = await readFile(path.resolve(process.cwd(), "package.json"), "utf-8");
-      const { version } = JSON.parse(data);
-      const content = `export default "${version}";\n`;
-
-      return await writeFile(
-        path.resolve(process.cwd(), config.root, "src/lib/helpers/version.ts"),
-        content
-      );
+    async configResolved() {
+      return await writeVersion();
     },
   };
 }
