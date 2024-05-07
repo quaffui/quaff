a3b259334a2a04ec33529b09856faa35b98f93c9
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-05-07 21:35:01 +0200

    feat(number): add a number utility

---
+++ src/lib/utils/number.ts
@@ -0,0 +1,7 @@
+export function isNumber(input: unknown): input is number {
+  return typeof input === "number" || !isNaN(Number(input));
+}
+
+export function between(value: number, min: number, max: number) {
+  return max <= min ? min : Math.min(max, Math.max(min, value));
+}
