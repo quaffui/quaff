44b2c798e09f43335eb3228298669e9eeab1fc17
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-05-07 21:35:00 +0200

    refactor(app.d.ts): add __Quaff__ namespace to ease types

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,6 +7,76 @@ declare global {
     // interface PageData {}
     // interface Platform {}
   }
+
+  namespace __Quaff__ {
+    export type Size = "none" | "xs" | "sm" | "md" | "lg" | "xl";
+
+    export type CssUnit = "px" | "%" | "em" | "ex" | "ch" | "rem" | "vw" | "vh" | "vmin" | "vmax";
+
+    export type CssValue = `${number}${CssUnit}`;
+
+    export interface QComponentDocs {
+      name: string;
+      description: string;
+      docs: {
+        props: QComponentProp[];
+        slots: QComponentSlot[];
+        methods: QComponentMethod[];
+        events: QComponentEvent[];
+      };
+    }
+
+    export interface QComponentProp {
+      name: string;
+      type: string;
+      default?: unknown;
+      description: string;
+      clickableType?: boolean;
+      optional?: boolean;
+    }
+
+    export interface QComponentSlot {
+      name: string;
+      description: string;
+    }
+
+    export interface QComponentType {
+      name: string;
+      description: string;
+    }
+
+    export interface QComponentEvent {
+      name: string;
+      type: string;
+      description: string;
+    }
+
+    export interface QComponentMethod {
+      name: string;
+      type: string;
+      description: string;
+    }
+
+    type ComponentName = `q-${string}`;
+
+    interface QuaffClassesParams {
+      bemClasses?: Record<string, unknown>;
+      classes?: unknown[];
+    }
+
+    /**
+     *
+     * @param componentName - Main class of the target element. The element should only have this class to be well targeted
+     * @param bemClasses - Classes that should be prefixed with `componentName`
+     * @param classes - Classes that should not be prefixed with `componentName`. Use arrow function if Svelte cries about $derived values
+     */
+    export function classes(
+      componentName: string,
+      { bemClasses, classes }: QuaffClassesParams
+    ): {
+      class: string;
+    };
+  }
 }
 
 export {};
