0d8d9a577d5b6bfe3d1644bb02c0ca5b7e41089f
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-05-07 21:35:01 +0200

    feat(utils): add a router utility using svelte stores

---
+++ src/lib/utils/router.ts
@@ -0,0 +1,34 @@
+import { page } from "$app/stores";
+import { derived } from "svelte/store";
+import type { Readable } from "svelte/store";
+
+export interface RouterProps {
+  activeClass?: string;
+  disabled?: boolean;
+  href?: string;
+  replace?: boolean;
+  to?: string;
+}
+
+export const isRouteActive: Readable<(route?: string) => boolean> = derived(page, ($page) => {
+  return (route?: string) =>
+    route === "/"
+      ? $page.url.pathname === route
+      : $page.url.pathname.slice(0, (route || "").length) === route;
+});
+
+export function getRouterInfo<T extends RouterProps>(props: T) {
+  const hasLink = [props.to, props.href].some((entry) => entry !== undefined);
+  const linkClasses = `${hasLink ? "q-link" : ""} ${props.disabled ? "disabled" : ""}`.trim();
+
+  const linkAttributes = {
+    href: props.to || props.href,
+    "data-sveltekit-reload": props.replace ? true : undefined,
+  };
+
+  return {
+    hasLink,
+    linkAttributes,
+    linkClasses,
+  };
+}
