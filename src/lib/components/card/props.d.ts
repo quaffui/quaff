fbfd134e7ae787d88293e88393acdaf6c2a18599
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-05-09 01:15:08 +0200

    refactor(card): migration to Svelte 5

---
+++ src/lib/components/card/props.d.ts
@@ -0,0 +1,53 @@
+import type { UseAlignProps } from "$lib/composables/useAlign";
+import type { HTMLAttributes } from "svelte/elements";
+import type { Snippet } from "svelte";
+
+export type CardFillColors = "primary" | "secondary" | "tertiary";
+
+export interface QCardProps extends HTMLAttributes<HTMLElement> {
+  /**
+   * Adds a border around the card.
+   * @default false
+   */
+  bordered?: boolean;
+
+  /**
+   * Defines the fill color of the card.
+   * @default false
+   */
+  fill?: boolean | CardFillColors;
+
+  /**
+   * Use the flat design for the card, removing its elevation.
+   * @default false
+   */
+  flat?: boolean;
+
+  /**
+   * Adds border radius to the card to round its corners.
+   * @default false
+   */
+  rounded?: boolean;
+
+  /**
+   * Sets the title of the card.
+   * @default undefined
+   */
+  title?: string | Snippet;
+}
+
+export interface QCardSectionProps extends HTMLAttributes<HTMLDivElement> {
+  /**
+   * Lays out the section content horizontally.
+   * @default false
+   */
+  horizontal?: boolean;
+}
+
+export interface QCardActionsProps extends UseAlignProps, HTMLAttributes<HTMLElement> {
+  /**
+   * Lays out the action items vertically.
+   * @default false
+   */
+  vertical?: boolean;
+}
