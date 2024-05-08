7ae819cc0ba930208c27d2483b82a5ade0313961
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-05-09 01:15:08 +0200

    refactor(card): migration to Svelte 5

--- src/lib/components/card/QCard.svelte
+++ src/lib/components/card/QCard.svelte
@@ -1,35 +1,44 @@
+<svelte:options runes={true} />
+
 <script lang="ts">
   import type { QCardProps } from "./props";
 
-  export let bordered: QCardProps["bordered"] = false,
-    fill: QCardProps["fill"] = undefined,
-    flat: QCardProps["flat"] = false,
-    round: QCardProps["round"] = false,
-    title: QCardProps["title"] = undefined,
-    userClasses: QCardProps["userClasses"] = "";
-  export { userClasses as class };
+  let {
+    title,
+    fill = false,
+    flat = false,
+    bordered = false,
+    rounded = false,
+    children,
+    ...props
+  }: QCardProps = $props();
 
   const colorOptions: (typeof fill)[] = ["primary", "secondary", "tertiary"];
 
-  $: color = !fill
-    ? "surface"
-    : colorOptions.includes(fill)
-    ? `${fill}-container`
-    : "surface-variant";
+  const color = $derived(
+    !fill ? "surface" : colorOptions.includes(fill) ? `${fill}-container` : "surface-variant"
+  );
+
+  __Quaff__.classes("q-card", {
+    bemClasses: {
+      flat,
+      bordered,
+      rounded,
+    },
+    classes: [color, props.class],
+  });
 </script>
 
-<article
-  class="q-card {color} {userClasses}"
-  class:q-card--bordered={bordered}
-  class:q-card--flat={flat}
-  class:q-card--rounded={round}
-  {...$$restProps}
->
-  {#if $$slots.title}
-    <slot name="title" />
-  {:else if title !== undefined}
+<article {...props} class="q-card" {...__Quaff__.classes}>
+  {#if typeof title === "string"}
     <h5 class="q-card__title">{title}</h5>
+  {:else}
+    {@render title?.()}
   {/if}
 
-  <slot />
+  {@render children?.()}
 </article>
+
+<style lang="scss">
+  @import "./QCard.scss";
+</style>
