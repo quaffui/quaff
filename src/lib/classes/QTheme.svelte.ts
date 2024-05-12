3b4d36c55ef0145daaa1d543cdc7c72d93a69175
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-05-12 21:04:36 +0200

    refactor: improve colors handling

--- src/lib/classes/QTheme.svelte.ts
+++ src/lib/classes/QTheme.svelte.ts
@@ -1,71 +1,32 @@
-import materialDynamicColors from "material-dynamic-colors";
-import QColors from "$lib/utils/colors";
-import type {
-  IMaterialDynamicColorsTheme,
-  IMaterialDynamicColorsThemeColor,
-} from "material-dynamic-colors/src/cdn/interfaces";
-import { convertCase } from "$lib/utils/string";
+import { generateColors } from "$lib/utils/colors";
+import type { HexValue, QuaffColors } from "$lib/utils/colors";
 
-interface MaterialColors {
-  primary: string;
-  "on-primary": string;
-  "primary-container": string;
-  "on-primary-container": string;
-  secondary: string;
-  "on-secondary": string;
-  "secondary-container": string;
-  "on-secondary-container": string;
-  tertiary: string;
-  "on-tertiary": string;
-  "tertiary-container": string;
-  "on-tertiary-container": string;
-  error: string;
-  "error-container": string;
-  "on-error": string;
-  "on-error-container": string;
-  background: string;
-  "on-background": string;
-  surface: string;
-  "on-surface": string;
-  "surface-variant": string;
-  "on-surface-variant": string;
-  outline: string;
-  "inverse-on-surface": string;
-  "inverse-surface": string;
-  "inverse-primary": string;
-  shadow: string;
-}
-
-interface MaterialColorsTheme {
-  light: MaterialColors;
-  dark: MaterialColors;
-}
-
-type ThemeColorName = `${keyof MaterialColors}-${keyof MaterialColorsTheme}`;
-type ThemeColors = Record<ThemeColorName, string>;
+type Mode = "light" | "dark";
+type ThemeColors = Record<`${keyof QuaffColors}-${Mode}`, HexValue>;
 
 function extractColorFromCssVar(cssVar: string) {
   const rootStyles = getComputedStyle(document.documentElement);
-  const varName = cssVar.replace(/var\(([a-z0-9-]+)\)/, "$1");
+  const varName = cssVar.replace(/var\(([a-z0-9-]+)\)/, "$1"); // remove var(...) to get only the color
   return rootStyles.getPropertyValue(varName).trim();
 }
 
-async function prepareThemeColors(from: string) {
+function prepareThemeColors(from: string) {
   if (from.startsWith("var(")) {
     from = extractColorFromCssVar(from);
   }
 
-  const theme = await materialDynamicColors(from);
+  const theme = generateColors(from);
+
+  console.log(theme);
 
   //@ts-expect-error The properties are added in the next for loop
   const themeColors: ThemeColors = {};
 
-  let mode: keyof IMaterialDynamicColorsTheme;
+  let mode: "light" | "dark";
   for (mode in theme) {
-    let color: keyof IMaterialDynamicColorsThemeColor;
+    let color: keyof QuaffColors;
     for (color in theme[mode]) {
-      const colorFormatted = convertCase(color, "camel", "kebab") as keyof MaterialColors;
-      const cssColor: ThemeColorName = `${colorFormatted}-${mode}`;
+      const cssColor: keyof typeof themeColors = `${color}-${mode}`;
       themeColors[cssColor] = theme[mode][color];
     }
   }
@@ -77,7 +38,7 @@ class QTheme {
   themeColors = $state({} as ThemeColors);
 
   constructor() {
-    prepareThemeColors("#3499E7").then((res) => (this.themeColors = res));
+    this.themeColors = prepareThemeColors("#0039b4");
   }
 
   private apply() {
@@ -86,21 +47,19 @@ class QTheme {
       return;
     }
 
-    let colorName: ThemeColorName;
+    let colorName: keyof ThemeColors;
     for (colorName in this.themeColors) {
-      root.style.setProperty(
-        `--${colorName}`,
-        QColors.hexToRgb(this.themeColors[colorName]).join(", ")
-      );
+      root.style.setProperty(`--${colorName}`, this.themeColors[colorName]);
     }
   }
 
-  updateThemeColor(color: ThemeColorName, newVal: string) {
+  updateThemeColor(color: keyof ThemeColors, newVal: HexValue) {
     this.themeColors[color] = newVal;
   }
 
-  async setTheme(from: string) {
-    const newTheme = await prepareThemeColors(from);
+  setTheme(from: string) {
+    console.log({ from });
+    const newTheme = prepareThemeColors(from);
     this.themeColors = newTheme;
     this.apply();
   }
