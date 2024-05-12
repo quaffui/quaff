b0a295e896ab0877eda6df44d6c0c7648ff398fb
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-05-12 15:03:21 +0200

    refactor(between): add a console warning if max < min

--- src/lib/utils/number.ts
+++ src/lib/utils/number.ts
@@ -3,5 +3,8 @@ export function isNumber(input: unknown): input is number | `${number}` {
 }
 
 export function between(value: number, min: number, max: number) {
+  if (max < min) {
+    console.warn(`max (${max}) should not be smaller than min (${min}).`);
+  }
   return max <= min ? min : Math.min(max, Math.max(min, value));
 }
