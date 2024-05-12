7deb9555e605adb0b46139a1cc9f726481bd1c4f
KilDesu <91275372+KilDesu@users.noreply.github.com>
2024-05-12 21:04:36 +0200

    refactor: improve colors handling

--- src/lib/utils/colors.ts
+++ src/lib/utils/colors.ts
@@ -1,4 +1,129 @@
-import { isNumber } from "./types";
+import {
+  TonalPalette,
+  argbFromHex,
+  hexFromArgb,
+  themeFromSourceColor,
+  type Theme,
+} from "@material/material-color-utilities";
+import { isNumber } from "./types.js";
+import { convertCase } from "./string.js";
+
+export type HexValue = `#${string}`;
+
+export type QuaffColors = {
+  primary: HexValue;
+  "on-primary": HexValue;
+  "primary-container": HexValue;
+  "on-primary-container": HexValue;
+  secondary: HexValue;
+  "on-secondary": HexValue;
+  "secondary-container": HexValue;
+  "on-secondary-container": HexValue;
+  tertiary: HexValue;
+  "on-tertiary": HexValue;
+  "tertiary-container": HexValue;
+  "on-tertiary-container": HexValue;
+  neutral: HexValue;
+  "on-neutral": HexValue;
+  "neutral-container": HexValue;
+  "on-neutral-container": HexValue;
+  "neutral-variant": HexValue;
+  "on-neutral-variant": HexValue;
+  "neutral-variant-container": HexValue;
+  "on-neutral-variant-container": HexValue;
+  error: HexValue;
+  "on-error": HexValue;
+  "error-container": HexValue;
+  "on-error-container": HexValue;
+  surface: HexValue;
+  "surface-dim": HexValue;
+  "surface-bright": HexValue;
+  "on-surface": HexValue;
+  "on-surface-variant": HexValue;
+  "surface-container-lowest": HexValue;
+  "surface-container-low": HexValue;
+  "surface-container": HexValue;
+  "surface-container-high": HexValue;
+  "surface-container-highest": HexValue;
+  "inverse-surface": HexValue;
+  "inverse-on-surface": HexValue;
+  "inverse-primary": HexValue;
+  outline: HexValue;
+  "outline-variant": HexValue;
+  scrim: HexValue;
+  shadow: HexValue;
+};
+
+export function generateColors(from: string): { light: QuaffColors; dark: QuaffColors } {
+  const argb = argbFromHex(from);
+  const palettes = themeFromSourceColor(argb).palettes;
+
+  return {
+    light: getColors(palettes, "light"),
+    dark: getColors(palettes, "dark"),
+  };
+}
+
+function getColors(palettes: Theme["palettes"], mode: "light" | "dark") {
+  const tones =
+    mode === "light"
+      ? {
+          base: 40,
+          onBase: 100,
+          baseContainer: 90,
+          onBaseContainer: 10,
+        }
+      : {
+          base: 80,
+          onBase: 20,
+          baseContainer: 30,
+          onBaseContainer: 90,
+        };
+
+  const getColor = (color: string, palette: TonalPalette) => {
+    color = convertCase(color, "pascal", "kebab");
+
+    return [
+      [color, palette.tone(tones.base)],
+      [`on-${color}`, palette.tone(tones.onBase)],
+      [`${color}-container`, palette.tone(tones.baseContainer)],
+      [`on-${color}-container`, palette.tone(tones.onBaseContainer)],
+    ];
+  };
+
+  const results: Record<keyof QuaffColors, number> = Object.fromEntries(
+    Object.entries(palettes)
+      .map(([color, palette]) => getColor(color, palette))
+      .flat(1)
+  );
+
+  results["surface"] = palettes.neutral.tone(mode === "light" ? 98 : 6);
+  results["surface-dim"] = palettes.neutral.tone(mode === "light" ? 87 : 6);
+  results["surface-bright"] = palettes.neutral.tone(mode === "light" ? 98 : 24);
+
+  results["on-surface"] = palettes.neutral.tone(mode === "light" ? 10 : 90);
+  results["on-surface-variant"] = palettes.neutralVariant.tone(mode === "light" ? 30 : 90);
+
+  results["surface-container-lowest"] = palettes.neutral.tone(mode === "light" ? 100 : 4);
+  results["surface-container-low"] = palettes.neutral.tone(mode === "light" ? 96 : 10);
+  results["surface-container"] = palettes.neutral.tone(mode === "light" ? 94 : 12);
+  results["surface-container-high"] = palettes.neutral.tone(mode === "light" ? 92 : 17);
+  results["surface-container-highest"] = palettes.neutral.tone(mode === "light" ? 90 : 24);
+
+  results["inverse-surface"] = palettes.neutral.tone(mode === "light" ? 20 : 90);
+  results["inverse-on-surface"] = palettes.neutral.tone(mode === "light" ? 95 : 20);
+  results["inverse-primary"] = palettes.primary.tone(mode === "light" ? 80 : 40);
+
+  results["outline"] = palettes.neutralVariant.tone(mode === "light" ? 50 : 60);
+  results["outline-variant"] = palettes.neutralVariant.tone(mode === "light" ? 80 : 30);
+
+  results["scrim"] = palettes.neutral.tone(0);
+  results["shadow"] = palettes.neutral.tone(0);
+
+  return Object.fromEntries(
+    Object.entries(results).map(([color, value]) => [color, hexFromArgb(value)])
+  ) as QuaffColors;
+}
 
 class QColors {
   private static hexRegex: RegExp = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
