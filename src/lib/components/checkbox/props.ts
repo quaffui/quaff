35d394df7bdbf314592eb9a1c8f701a3258f9ad5
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2024-05-17 11:17:58 +0200

    refactor(QCheckbox): migrate to Svelte 5.

--- src/lib/components/checkbox/props.ts
+++ src/lib/components/checkbox/props.ts
@@ -1,7 +1,6 @@
-import type { NativeProps } from "$lib/utils";
 import type { HTMLAttributes } from "svelte/elements";
 
-export interface QCheckboxProps extends NativeProps, HTMLAttributes<HTMLLabelElement> {
+export interface QCheckboxProps extends HTMLAttributes<HTMLLabelElement> {
   /**
    * Controls the checked state of the checkbox.
    */
