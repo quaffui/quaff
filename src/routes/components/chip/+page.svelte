7c05c94b45730068d786c35d02e44d04b69c6f21
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2024-05-23 20:00:27 +0200

    refactor(QDocs): migrate to Svelte 5, update related code.

--- src/routes/components/chip/+page.svelte
+++ src/routes/components/chip/+page.svelte
@@ -7,50 +7,54 @@
 </script>
 
 <QDocs componentDocs={QChipDocs}>
-  <QChip slot="display" elevated icon="img:/cocktail.jpg" label="Cocktail" />
+  {#snippet display()}
+    <QChip elevated icon="img:/cocktail.jpg" label="Cocktail" />
+  {/snippet}
 
-  <div slot="usage">
-    <QDocsSection {snippets} title="Default">
-      <div class="flex q-gap-lg items-center">
-        <QChip class="bg-green" label="Green Chip" />
-        <QChip class="error" label="Error Chip" />
-        <QChip class="primary" label="Primary Chip" />
-        <QChip class="primary-container" label="Primary Container Chip" />
-      </div>
-    </QDocsSection>
+  {#snippet usage()}
+    <div>
+      <QDocsSection {snippets} title="Default">
+        <div class="flex q-gap-lg items-center">
+          <QChip class="bg-green" label="Green Chip" />
+          <QChip class="error" label="Error Chip" />
+          <QChip class="primary" label="Primary Chip" />
+          <QChip class="primary-container" label="Primary Container Chip" />
+        </div>
+      </QDocsSection>
 
-    <QDocsSection {snippets} title="Chip Sizes">
-      <div class="flex q-gap-lg items-center">
-        <QChip label="Small Chip" size="sm" icon="mail" />
-        <QChip label="Medium Chip" size="md" icon="mail" />
-        <QChip label="Large Chip" size="lg" icon="mail" />
-      </div>
-    </QDocsSection>
+      <QDocsSection {snippets} title="Chip Sizes">
+        <div class="flex q-gap-lg items-center">
+          <QChip label="Small Chip" size="sm" icon="mail" />
+          <QChip label="Medium Chip" size="md" icon="mail" />
+          <QChip label="Large Chip" size="lg" icon="mail" />
+        </div>
+      </QDocsSection>
 
-    <QDocsSection {snippets} title="Chip designs">
-      <div class="flex q-gap-lg items-center">
-        <QChip icon="map" label="Assist (default)" />
-        <QChip kind="filter" icon="filter_list" label="Filter" />
-        <QChip kind="input" icon="edit" trailingIcon="close" label="Input" />
-        <QChip kind="suggestion" icon="help" label="Suggestion" />
-        <QChip disabled icon="warning" label="Disabled" />
-      </div>
-      <QSeparator text="Elevated designs" textAlign="left" spacing="lg" />
-      <div class="flex q-gap-lg items-center">
-        <QChip elevated icon="map" label="Assist (default)" />
-        <QChip elevated kind="filter" icon="filter_list" label="Filter" />
-        <QChip elevated kind="input" icon="edit" trailingIcon="close" label="Input" />
-        <QChip elevated kind="suggestion" icon="help" label="Suggestion" />
-        <QChip elevated disabled icon="warning" label="Disabled" />
-      </div>
-    </QDocsSection>
+      <QDocsSection {snippets} title="Chip designs">
+        <div class="flex q-gap-lg items-center">
+          <QChip icon="map" label="Assist (default)" />
+          <QChip kind="filter" icon="filter_list" label="Filter" />
+          <QChip kind="input" icon="edit" trailingIcon="close" label="Input" />
+          <QChip kind="suggestion" icon="help" label="Suggestion" />
+          <QChip disabled icon="warning" label="Disabled" />
+        </div>
+        <QSeparator text="Elevated designs" textAlign="left" spacing="lg" />
+        <div class="flex q-gap-lg items-center">
+          <QChip elevated icon="map" label="Assist (default)" />
+          <QChip elevated kind="filter" icon="filter_list" label="Filter" />
+          <QChip elevated kind="input" icon="edit" trailingIcon="close" label="Input" />
+          <QChip elevated kind="suggestion" icon="help" label="Suggestion" />
+          <QChip elevated disabled icon="warning" label="Disabled" />
+        </div>
+      </QDocsSection>
 
-    <QDocsSection {snippets} title="Chips with Images">
-      <div class="flex q-gap-lg items-center">
-        <QChip icon="img:/cocktail.jpg" label="Leading avatar" />
-        <QChip kind="filter" elevated icon="img:/cocktail.jpg" label="Filter elevated" />
-        <QChip kind="input" icon="img:/cocktail.jpg" label="Input chip with avatar" />
-      </div>
-    </QDocsSection>
-  </div>
+      <QDocsSection {snippets} title="Chips with Images">
+        <div class="flex q-gap-lg items-center">
+          <QChip icon="img:/cocktail.jpg" label="Leading avatar" />
+          <QChip kind="filter" elevated icon="img:/cocktail.jpg" label="Filter elevated" />
+          <QChip kind="input" icon="img:/cocktail.jpg" label="Input chip with avatar" />
+        </div>
+      </QDocsSection>
+    </div>
+  {/snippet}
 </QDocs>
--- src/routes/components/progress/+page.svelte
+++ src/routes/components/progress/+page.svelte
@@ -7,24 +7,26 @@
 </script>
 
 <QDocs componentDocs={QProgressDocs}>
-  <div slot="usage">
-    <QDocsSection title="Linear Progress">
-      <QLinearProgress value={30}></QLinearProgress>
-      <QLinearProgress class="q-mt-md" value={30} reverse></QLinearProgress>
-      <QLinearProgress class="q-mt-md" value={30}></QLinearProgress>
-      <QLinearProgress class="q-mt-md" indeterminate></QLinearProgress>
-    </QDocsSection>
+  {#snippet usage()}
+    <div>
+      <QDocsSection title="Linear Progress">
+        <QLinearProgress value={30}></QLinearProgress>
+        <QLinearProgress class="q-mt-md" value={30} reverse></QLinearProgress>
+        <QLinearProgress class="q-mt-md" value={30}></QLinearProgress>
+        <QLinearProgress class="q-mt-md" indeterminate></QLinearProgress>
+      </QDocsSection>
 
-    <QDocsSection title="Circular Progress">
-      <QCircularProgress color="primary" value={10}></QCircularProgress>
-      <QCircularProgress color="secondary" class="q-ml-md" value={90}></QCircularProgress>
-      <QCircularProgress color="tertiary" class="q-ml-md" value={80} indeterminate
-      ></QCircularProgress>
-    </QDocsSection>
-    <QDocsSection title="Circular Progress sizes">
-      <QCircularProgress color="primary" value={30} size="1rem" />
-      <QCircularProgress color="secondary" class="q-ml-md" value={90} size="3rem" />
-      <QCircularProgress color="tertiary" class="q-ml-md" indeterminate size="5rem" />
-    </QDocsSection>
-  </div>
+      <QDocsSection title="Circular Progress">
+        <QCircularProgress color="primary" value={10}></QCircularProgress>
+        <QCircularProgress color="secondary" class="q-ml-md" value={90}></QCircularProgress>
+        <QCircularProgress color="tertiary" class="q-ml-md" value={80} indeterminate
+        ></QCircularProgress>
+      </QDocsSection>
+      <QDocsSection title="Circular Progress sizes">
+        <QCircularProgress color="primary" value={30} size="1rem" />
+        <QCircularProgress color="secondary" class="q-ml-md" value={90} size="3rem" />
+        <QCircularProgress color="tertiary" class="q-ml-md" indeterminate size="5rem" />
+      </QDocsSection>
+    </div>
+  {/snippet}
 </QDocs>
