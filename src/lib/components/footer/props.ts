c7607c86d20cab71ec2d02c5248ff75b36a801f0
coder1337 <78889972+therealcoder1337@users.noreply.github.com>
2026-09-13 21:19:47 +0200

    feat(QFooter): align with md3 specs

--- src/lib/components/footer/props.ts
+++ src/lib/components/footer/props.ts
@@ -9,12 +9,12 @@ export interface QFooterProps
   reveal?: boolean;
 
   /**
-   * The offset in pixels to trigger the reveal effect. The footer will be hidden when the scroll position is greater than this value.
+   * The distance in pixels from the bottom of the layout content at which the footer is revealed.
    */
   revealOffset?: number;
 
   /**
-   * The height of the footer. Can be specified with a CSS unit. If not specified, "px" will be used. (specified CSS units are not supported yet)
+   * The height of the footer in pixels.
    */
   height?: number;
 }
